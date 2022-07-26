9e8972e3689e392d5d30fa8120d9b2b3bb581e11
alimuhammadmz <alimuhammad.moaiz@gmail.com>
2022-07-26 18:08:37 +0500

    Authentication for routes implemented

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const user = require('../models/userModel');
+
+const isAuthenticUser = async (req, res, next) =>{
+    const {token} = req.cookies;
+    if(!token){
+        return res.status(401).json({'message':"Please login to continue!"});    
+    }
+
+    const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+    const User = await user.findById(decodedData.id);
+    console.log(User);
+    next();
+    
+}
+
+module.exports.isAuthenticUser = isAuthenticUser;
